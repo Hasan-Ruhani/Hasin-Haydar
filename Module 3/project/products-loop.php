6331d3259eb2dd74d3012201c8c2b84b6fe3e8d5
H A S A N <hasan25042019@gmail.com>
2023-02-15 21:50:46 +0600

    Update products-loop.php
    vcv

--- Module 3/project/products-loop.php
+++ Module 3/project/products-loop.php
@@ -17,17 +17,9 @@
                 <div class="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
                 <?php
                     include_once("data.php");
+                    include_once("library.php");
                     foreach($products as $product){
-                ?>
-                <a href="#" class="group">
-                    <div class="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
-                        <img src="<?php echo $product['image'] ?>" class="h-full w-full object-cover object-center group-hover:opacity-75"> 
-                        <!-- alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." -->
-                    </div>
-                    <h3 class="mt-4 text-sm text-gray-700"><?php echo $product['name']; ?></h3>
-                    <p class="mt-1 text-lg font-medium text-gray-900"><?php echo $product['price']; ?></p>
-                </a>
-                <?php
+                        displayProduct($product);
                     }
                 ?>
                 </div>

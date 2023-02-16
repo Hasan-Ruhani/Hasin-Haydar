8c0d650141a2bea909e50b1c6e671ea28a3694e7
H A S A N <hasan25042019@gmail.com>
2023-02-16 20:37:22 +0600

    Update products-loop.php
    gh

--- Module 3/project/products-loop.php
+++ Module 3/project/products-loop.php
@@ -8,7 +8,6 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="antialiased font-sans">
-
         <div class="bg-white">
 
             <div class="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">

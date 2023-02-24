c1f191c38ddbfd17669211fa85b9841a54472310
H A S A N <hasan25042019@gmail.com>
2023-02-24 11:55:24 +0600

    Update encrypt.php
    gfg

--- Module 4/practice/encrypt.php
+++ Module 4/practice/encrypt.php
@@ -1,20 +1,29 @@
 <?php
-    $origin = "abcdefghijklmnopqrstuvwxyz0123456789";
-    $key = "mge5prcfv0u3ohq8ilntx9wz4ak1y7ds6jb2";
 
-    $message = "Hey guy, aru you from Noakhali";
-    $message = strtolower($message);
-    $output = '';
+    if(isset($_GET['submitButton'])){
+        $origin = "abcdefghijklmnopqruvwxyz0123456789";
+        $key = "9qbiwn30rpy285jf1emxu6v7olg4dkchza";
 
-    for($i = 0; $i < strlen($message); $i++){
-        $character = $message[$i];
-        $position = strpos($origin, $character);
-        if($position !== false){
-            $replace = $key[$position];
+        $message = $_GET['message'];
+        $message = strtolower($message);
+        $output = '';
+        
+        for($i = 0; $i < strlen($message); $i++){
+            $character = $message[$i];
+
+            $position = strpos($origin, $character);
+
+            if($position !== false){
+                $replace = $key[$position];
+            }
+            else{
+                $replace = $character;
+            }
+            $output = $output . $replace;
         }
-        else{
-            $replace = $character;
-        }
-        $output = $output . $replace;
+
     }
-    echo $output;
+
+    else{
+        $output = "You are not Encrypt any string!!";
+    }

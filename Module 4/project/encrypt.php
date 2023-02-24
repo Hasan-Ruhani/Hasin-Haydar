155ef6388148d4d9af1f318c7b843f72f49b5760
H A S A N <hasan25042019@gmail.com>
2023-02-24 11:55:42 +0600

    Update encrypt.php
    fgf

--- Module 4/project/encrypt.php
+++ Module 4/project/encrypt.php
@@ -1,20 +1,27 @@
 <?php
-    $origin = "abcdefghijklmnopqrstuvwxyz0123456789";
-    $key = "mge5prcfv0u3ohq8ilntx9wz4ak1y7ds6jb2";
+    if (isset($_GET['submitButton'])) {
+        // Get the value of the input field
+        $origin = "abcdefghijklmnopqrstuvwxyz0123456789";
+        $key = "mge5prcfv0u3ohq8ilntx9wz4ak1y7ds6jb2";
+        
+        $message = $_GET['message'];
+    // $message = "Hey guy, aru you from Noakhali";
+        $message = strtolower($message);
+        $output = '';
 
-    $message = "Hey guy, aru you from Noakhali";
-    $message = strtolower($message);
-    $output = '';
-
-    for($i = 0; $i < strlen($message); $i++){
-        $character = $message[$i];
-        $position = strpos($origin, $character);
-        if($position !== false){
-            $replace = $key[$position];
+        for($i = 0; $i < strlen($message); $i++){
+            $character = $message[$i];
+            $position = strpos($origin, $character);
+            if($position !== false){
+                $replace = $key[$position];
+            }
+            else{
+                $replace = $character;
+            }
+            $output = $output . $replace;
         }
-        else{
-            $replace = $character;
-        }
-        $output = $output . $replace;
     }
-    echo $output;
+
+    else{
+        $output = "You are not Encrypt any text!!";
+    }

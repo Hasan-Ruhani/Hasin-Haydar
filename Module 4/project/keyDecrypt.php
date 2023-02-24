b4b20643bc2aeaae30cc2c6c0ca4800e4b4b88d7
H A S A N <hasan25042019@gmail.com>
2023-02-24 11:55:45 +0600

    Update keyDecrypt.php
    fgf

--- Module 4/project/keyDecrypt.php
+++ Module 4/project/keyDecrypt.php
@@ -9,25 +9,7 @@
     <title>Document</title>
 </head>
 <?php 
-    if (isset($_GET['submitButton'])) {
-        // Get the value of the input field
-       $origin = "abcdefghijklmnopqrstuvwxyz0123456789";
-        $key = "mge5prcfv0u3ohq8ilntx9wz4ak1y7ds6jb2";
-        
-        $message = $_GET['message'];
-    $output = '';
-    for($i=0;$i<strlen($message);$i++){
-        $character = $message[$i];
-        $position = strpos($key,$character);
-        if($position!==false){
-            $replacement = $origin[$position];
-        }else{
-            $replacement = $character;
-        }
-        
-        $output = $output . $replacement;
-    }
-}
+    include_once("deCrypt.php");
 ?>
 
 <body>
@@ -35,8 +17,8 @@
     <section class="text-gray-600 body-font relative">
         <div class="container px-5 py-24 mx-auto">
             <div class="flex flex-col text-center w-full mb-12">
-                <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Generate Keys</h1>
-                <p class="lg:w-2/3 mx-auto leading-relaxed text-base">generate Random String Key</p>
+                <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Decrypt Keys</h1>
+                <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Decrypt Random String Key</p>
             </div>
             <div class="flex flex-col text-center w-full mb-12">
                 <p class="lg:w-2/3 mx-auto leading-relaxed text-base"><?php echo $output; ?></p>
@@ -48,7 +30,7 @@
                         <div class="p-2 w-full">
                             <div class="relative">
 
-                                <label for="message" class="leading-7 text-sm text-gray-600">Click the generate key button</label>
+                                <label for="message" class="leading-7 text-sm text-gray-600">Click the decrypt key button</label>
                                 
                                     <input type="textarea" id="message" name="message" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out">
                                     </div>

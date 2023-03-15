488a2714eb383e3f11ca23be4d0d56a48de6205f
H A S A N <hasan25042019@gmail.com>
2023-03-15 19:24:11 +0600

    Delete fileWrite.php
    rtrt

--- Module 6/fileWrite.php
+++
@@ -1,9 +0,0 @@
-<?php  
-    
-    // $file = fopen("C:\\xampp\\htdocs\\Hasin-Haydar\\Module 6\\fileWrite.txt", "w");  //write and clean previous data
-    $file = fopen("C:\\xampp\\htdocs\\Hasin-Haydar\\Module 6\\fileWrite.txt", "a");  //write all dat5 
-    fwrite($file, "\n Hello World");
-
-    // file_put_contents("C:\\xampp\\htdocs\\Hasin-Haydar\\Module 6\\fileWrite.txt", 5);            //clear previous all data and set current data
-    file_put_contents("C:\\xampp\\htdocs\\Hasin-Haydar\\Module 6\\fileWrite.txt", "\n 5", FILE_APPEND);     //echo previous all data and set current data
-    fclose($file);
